a18af575ee0df03be68f1d5e96490c2ec984cc95
Admin <Admin@JIVIN-DIYA-PC>
2024-10-06 23:56:12 +0530

    Add project files.

---
+++ ErrorHandlingApplication/DivNumbers.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorHandlingApplication
+{
+    internal class DivNumbers
+    {
+        int result;
+
+       public DivNumbers()
+        {
+            result = 0;
+        }
+        public void division(int num1, int num2)
+        {
+            try
+            {
+                result = num1 / num2;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("Exception caught: {0}", e);
+            }
+            finally
+            {
+                Console.WriteLine("Result: {0}", result);
+            }
+        }
+    }
+}
---
+++ ErrorHandlingApplication/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ErrorHandlingApplication;
+
+DivNumbers d = new();
+d.division(25, 0);
+Console.ReadKey();
